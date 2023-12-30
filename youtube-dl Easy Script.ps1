793680a4d7ce2d9196a4d11eb7a98e0bc6f19156
ThioJoe <12518330+ThioJoe@users.noreply.github.com>
2023-12-30 16:22:25 -0700

    Add option to download specific single file
    Added option to download a specific pre-existing single filetype of combined audio/video. Turns out it's basically the same command as 5 but I realized that late. This at least gives better instructions based on what the user wants to do.
    
    Also changed some default variable values. Now assumes ffmpeg is in current directory. Also outputs video files to "Outputs" folder in current directory instead of desktop.
    
    Also set yt-dlp.exe as default

--- youtube-dl Easy Script.ps1
+++ youtube-dl Easy Script.ps1
@@ -19,9 +19,9 @@
 # Set ffmpeg location here. Make sure it is up to date (if using chocolatey:  chocolatey upgrade ffmpeg )
 # Set output location and filename of downloaded files. Defaults to Desktop, with video title and video extension. See documentation on specifics.
 # Set default options / parameters to apply to all downloads. See youtube-dl documentation for details. Includes ffmpeg location and output location using the other variables.
-$ffmpeg_location="`"C:\Program Files\ffmpeg\bin\ffmpeg.exe`""
-$output_location="`"$HOME\Desktop\%(title)s.%(ext)s`""
-$downloader_exe="youtube-dl.exe" # "yt-dlp.exe"  or  "youtube-dl.exe"
+$ffmpeg_location="`"ffmpeg.exe`"" # Just put it in the same directory as the script
+$output_location="`"Outputs\%(title)s.%(ext)s`"" # Outputs to a folder called "Outputs" in the same directory as the script, with filename as video title
+$downloader_exe="yt-dlp.exe" # "yt-dlp.exe"  or  "youtube-dl.exe"
 $options="--no-mtime --ffmpeg-location $ffmpeg_location --output $output_location"
 
 #################### Functions ####################
@@ -38,11 +38,13 @@ function Format-PathVariable($path) {
 function Set-Format {
 	Switch ($choice)
 	{
-		1 {Write-Output $null}
-		2 {Write-Output "-f best"}
-		3 {Write-Output "-f bestvideo+bestaudio/best --merge-output-format mp4"}
-		4 {Write-Output -f $format --merge-output-format mp4}
-		5 {Write-Output -f $format}
+		1 {Write-Output $null} # Automatic default is best video + audio muxed
+		2 {Write-Output "-f best"} # Best quality audio+video single file, no mux
+		3 {Write-Output "-f bestvideo+bestaudio/best --merge-output-format mp4"} # Choose highest quality video and audio formats to combine
+		4 {Write-Output -f $format --merge-output-format mp4} # Choose video and audio formats to combine
+		5 {Write-Output -f $format} # Download only audio or video
+		6 {Write-Output "-f $format"} # Specify single audio+video file
+		# Note for later: Optiosn 5 and 6 likely need to be combined into one option, since they are the same thing
 	}
 }
 
@@ -63,12 +65,17 @@ function Custom-Formats {
 		$chosenFormat = ${videoFormat}+"+"+${audioFormat}
 		Write-Output $chosenFormat #Returns this variable out of the function
 	}
-	else { if ($choice -eq 5) {Write-Host "INSTRUCTIONS: Choose the format code for the video or audio quality you want from the list at the top."
+	elseif ($choice -eq 5) {Write-Host "INSTRUCTIONS: Choose the format code for the video or audio quality you want from the list at the top."
+		$chosenFormat = Read-Host "Format Code"
+		Write-Output $chosenFormat #Returns this variable out of the function
+		 }
+	
+	elseif ($choice -eq 6) {Write-Host "INSTRUCTIONS: Choose the format code for a specific single audio+video file (one that DOESN'T say 'video only' or 'audio only')."
 		$chosenFormat = Read-Host "Format Code"
 		Write-Output $chosenFormat #Returns this variable out of the function
 		 }
 	}
-}
+
 
 # Updates youtube-dl (must be in same directory as script)
 function Update-Program{
@@ -94,22 +101,23 @@ $URL = Read-Host "Enter video URL here"
 Write-Output ""
 & .\$downloader_exe --list-formats $URL
 
-# While loop until user is satisfied with output and confirms using Check-Format function
 while ($confirm -ne "y") {
 	Write-Output ""
 	Write-Output "---------------------------------------------------------------------------"
 	Write-Output "Options:"
 	Write-Output "1. Download automatically (default is best video + audio muxed)"
-	Write-Output "2. Download the best quality single file, no mux"
+	Write-Output "2. Download the best quality audio+video single file, no mux"
 	Write-Output "3. Download the highest quality audio + video formats, attempt merge to mp4"
-	Write-Output "4. Let me choose the video and audio formats to combine"
+	Write-Output "4. Let me individually choose the video and audio formats to combine"
 	Write-Output "5. Download ONLY audio or video"
-	Write-Output "6. -UPDATE PROGRAM- (Admin May Be Required)"
+	Write-Output "6. Download a specific audio+video single file, no mux"
+	Write-Output "7. -UPDATE PROGRAM- (Admin May Be Required)"
 	Write-Output ""
 
-	$choice = Read-Host "Type your choice number" #Takes in choice from user
-	if (($choice -eq 4)	-or ($choice -eq 5)) { $format = Custom-Formats }
-	if ($choice -eq 6) {Update-Program}
+	$choice = Read-Host "Type your choice number"
+	if (($choice -eq 4) -or ($choice -eq 5) -or ($choice -eq 6)) { $format = Custom-Formats }
+	if ($choice -eq 7) {Update-Program}
+	# if ($choice -eq 6) {$id = Read-Host "Enter format ID"}
 	$format = Set-Format
 	$confirm = Check-Format
 }
@@ -117,6 +125,6 @@ while ($confirm -ne "y") {
 # Final Run
 Write-Output ""
 Write-Output "Running Command:   .\$downloader_exe $format $URL '--%' $options"
-& .\$downloader_exe $format $URL '--%' $options #Final full command used on youtube-dl. I have no idea why '--%' is required in there but without it, it won't work. Got it from an obscure StackOverflow comment
+& .\$downloader_exe $format $URL '--%' $options #Final full command used on youtube-dl. The '--%' basically tells powershell not to interpret the rest of the line as powershell commands, so it can be passed to youtube-dl as is.
 cmd /c pause
 
